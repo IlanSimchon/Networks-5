eaf007dd12cba4f6ca07b14726794bd9f113f4b9
Ilan <smilan273@gmail.com>
2023-06-14 00:44:54 +0300

    fix part C

--- traceroute.py
+++ traceroute.py
@@ -2,30 +2,32 @@ from scapy.all import *
 from scapy.layers.inet import IP, ICMP, UDP, TCP
 
 
-def snoofer(dest_ip):
+def traceroute(dest_ip):
     ttl = 1
     count = 0
     temp_ip = ''
 
     while temp_ip != dest_ip:
-
+        # Create an ICMP packet with the specified destination IP and TTL
         packet = IP(dst=dest_ip, ttl=ttl) / ICMP()
 
-        print(packet.src)
-
+        # Send the packet and wait for a response
         response = sr1(packet, filter='icmp', timeout=2)
 
         if response and IP in response:
+            # If a response is received and it contains IP layer
             temp_ip = response[IP].src
             count += 1
             ttl += 1
         else:
             temp_ip = dest_ip
 
+    # Print the number of routers encountered and the source and destination IPs
     print(f"{count} numbers of routers between {packet.src} to {dest_ip}")
 
 
 if __name__ == '__main__':
     dest_ip = '8.8.8.8'
 
-    snoofer(dest_ip)
+    # Perform traceroute to the specified destination IP
+    traceroute(dest_ip)
