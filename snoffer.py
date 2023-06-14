be5aeeafc98a057bd80c800c0b99b5b32c134e67
Ilan <smilan273@gmail.com>
2023-06-14 10:55:08 +0300

    part D process

---
+++ snoffer.py
@@ -0,0 +1,17 @@
+from scapy.all import *
+from scapy.layers.inet import ICMP, IP
+
+fake_ip = '1.2.3.4'
+
+def snoffer(packet):
+    if packet[ICMP].type == 8:
+        packet = IP(src=packet[IP].dst ,dst =packet[IP].src)
+        icmp = ICMP(type=0 , code=0)
+
+        icmp_pack = packet / icmp
+
+        send(icmp_pack)
+
+
+if __name__ == '__main__':
+    sniff(filter='icmp', prn=snoffer , iface='br-9aeb3f816ce2')
